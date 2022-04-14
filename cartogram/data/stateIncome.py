2e8503e0efd5ffc7d0cbd219640aa4a065e8d8b8
root <root@DESKTOP-8JU170O.localdomain>
2022-04-14 18:59:44 -0400

    Rearrange files

---
+++ cartogram/data/stateIncome.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import pandas as pd
+
+df = pd.read_csv("Census_Population.csv")
+
+df2 = df[df['Label (Grouping)'] == '            Mean earnings (dollars)']
+#print(df2)
+
+df3 = df2.filter(regex='!!Total!!Estimate').fillna(0)
+newcols = [col.replace('!!Total!!Estimate', '') for col in df3.columns]
+df3.columns = newcols
+df4 = df3.apply(lambda x: x.astype(str).str.replace(',','').apply(pd.to_numeric))
+
+df4.to_csv("stateIncome.csv", index=False)
+df4.to_json("stateIncome.json", orient='records')
+
